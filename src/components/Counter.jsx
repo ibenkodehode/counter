8a830a8c443d1e91e16d2dcc3448d4def5865e22
Iben Kodehode <112859791+ibenkodehode@users.noreply.github.com>
2022-12-10 13:27:40 +0100

    feat/count component and name change

---
+++ src/components/Counter.jsx
@@ -0,0 +1,17 @@
+// import the useState Hook from React
+import React, { useState } from "react";
+
+function Counter() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div>
+      <p>{count}</p>
+      <button onClick={() => setCount(count + 1)}>Increment</button>
+      <button onClick={() => setCount(count - 1)}>Decrement</button>
+      <button onClick={() => setCount(0)}>Clear</button>
+    </div>
+  );
+}
+
+export default Counter;
