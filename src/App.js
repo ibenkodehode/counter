d1f2559892366ec5d60e47f39cb8c783d330f2c5
Iben Kodehode <112859791+ibenkodehode@users.noreply.github.com>
2022-12-15 09:30:11 +0100

    feat/ removed blobs

--- src/App.js
+++ src/App.js
@@ -9,8 +9,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
       </header>
       <main>
-        {" "}
-        <Counter />{" "}
+        <Counter />
       </main>
       <footer className="App-footer">
         <a
