5ad96f4ea4f77aadf198cafd48adbb11562b6c40
Iben Kodehode <112859791+ibenkodehode@users.noreply.github.com>
2022-12-15 09:27:01 +0100

    feat/ changed count to prevCount to avoid bugs

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -7,9 +7,13 @@ function Counter() {
   return (
     <div>
       <p className="result">{count}</p>
-      <button onClick={() => setCount(count + 1)}>Increment</button>
+      <button onClick={() => setCount((prevCount) => prevCount + 1)}>
+        Increment
+      </button>
       <button onClick={() => setCount(0)}>Clear</button>
-      <button onClick={() => setCount(count - 1)}>Decrement</button>
+      <button onClick={() => setCount((prevCount) => prevCount - 1)}>
+        Decrement
+      </button>
     </div>
   );
 }
